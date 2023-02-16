3bc229e8b214daeb819d7a4009849e1c41b42992
jody-fraser <jodykfraser@gmail.com>
2023-02-16 20:11:54 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -11,8 +11,9 @@ resource "azurerm_resource_group" "myresourcegroup" {
   }
 }
 
-module "network" {
-  source              = "Azure/network/azurerm"
+module "network" {module "network" {
+  source  = "app.terraform.io/dany-test/network/azurerm"
+  version = "5.1.0"
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
