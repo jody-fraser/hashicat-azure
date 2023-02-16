16d4ab6c4fd9b660c89a5e10de4b9732b311ce77
jody-fraser <jodykfraser@gmail.com>
2023-02-16 20:25:21 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -12,7 +12,7 @@ module "network" {
     "subnet2" : ["Microsoft.Sql"],
     "subnet3" : ["Microsoft.Sql"]
   }
-  use_for_each = true
+  
   tags = {
     environment = "dev"
     costcenter  = "it"
